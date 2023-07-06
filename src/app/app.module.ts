3a91cbfa08ff2b78ab31a9002b553f19833d3e8d
William Bucher <wjbuhcer3@gmail.com>
2023-07-05 23:13:05 -0400

    added basic layout for homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomepageComponent } from './components/homepage-components/homepage/homepage.component';
+import { ContactComponent } from './components/contact-components/contact/contact.component';
+import { GalleryComponent } from './components/gallery-components/gallery/gallery.component';
+import { TitleBannerComponent } from './components/homepage-components/title-banner/title-banner.component';
+import { AboutMeComponent } from './components/homepage-components/about-me/about-me.component';
+import { GalleryPanelComponent } from './components/homepage-components/gallery-panel/gallery-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ContactComponent,
+    GalleryComponent,
+    TitleBannerComponent,
+    AboutMeComponent,
+    GalleryPanelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/homepage-components/gallery-panel/gallery-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-panel',
+  templateUrl: './gallery-panel.component.html',
+  styleUrls: ['./gallery-panel.component.css']
+})
+export class GalleryPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
