51f1fb59e7f962f60a048ea9003f8219c3fc7ec7
William Bucher <wjbuhcer3@gmail.com>
2023-07-09 22:41:53 -0400

    cleaned up homepage a lil bit and added beginning view to gallery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage-components/homepage/homepage.component';
+import { GalleryComponent } from './components/gallery-components/gallery/gallery.component';
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent }
+  { path: 'home', component: HomepageComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },
+  
 ];
 
 @NgModule({
--- src/app/components/gallery-components/gallery/gallery.component.ts
+++ src/app/components/gallery-components/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Image } from '../../models/image';
 
 @Component({
   selector: 'app-gallery',
@@ -8,6 +9,25 @@ import { Component, OnInit } from '@angular/core';
 export class GalleryComponent implements OnInit {
 
   constructor() { }
+
+
+  // maybe use this? 
+  // https://www.npmjs.com/package/teqnia-ng-image-fullscreen-view
+  
+  images: Image[] = [
+    {path: "assets/photos/gallery/6S9A1990.jpg", description: "Mockingbird with Blueberry"},
+    {path: "assets/photos/gallery/6S9A2716.jpg", description: "Mockingbird in Fligh"},
+    {path: "assets/photos/gallery/6S9A2850.jpg", description: "Great Blue Heron"},
+    {path: "assets/photos/gallery/6S9A1309.jpg", description: "Cardinal"},
+    {path: "assets/photos/gallery/6S9A3320.jpg", description: "Dark Eyed Junco"},
+
+    {path: "assets/photos/6S9A7311.jpg", description: "House Finch in the Tree"},
+    {path: "assets/photos/yellow-bellied-sapsucker.jpg", description: "Yellow Bellied Sapsucker"},
+    {path: "assets/photos/6S9A6006.jpg", description: "Cardinal"},
+    {path: "assets/photos/6S9A7140.jpg", description: "Osprey with Fish"},
+
+
+  ]
 
   ngOnInit(): void {
   }
